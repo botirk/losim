574a9a4873beaee4c4051bb05c4f4dfce42d9cc6
Ivan <antipchel@gmail.com>
2024-01-31 22:51:28 +0300

    day 21 - browser ready

--- src/bin/simulateBestItems.ts
+++ src/bin/simulateBestItems.ts
@@ -1,50 +1,110 @@
-import readline from "node:readline/promises";
-import { stdin, stdout } from "node:process";
 import { champions } from "../champions/champions";
-import { simulateBestNextItem, simulateBestBoot, simulateBestNextItems } from "../simulation/simulateBestItems";
+import { simulateBestNextItems } from "../simulation/simulateBestItems";
 import { Champion } from "../champions/champion/champion";
 import { MasterYi } from "../champions/MasterYi/MasterYi";
 
-const rli = readline.createInterface(stdin, stdout);
 
-(async () => {
+
+interface SimulateBestItemsSetup {
+  Champion: new() => Champion,
+  itemsCount: number,
+  level: number,
+  shouldRunAway: boolean,
+  withBoots: boolean,
+}
+
+const getSetupNode = async (): Promise<SimulateBestItemsSetup> => {
+  const { stdin, stdout } = require("node:process");
+  const rli = require("node:readline/promises").createInterface(stdin, stdout);
+
   for (const i in champions) rli.write(`${i} ${new champions[i]().name}\n`);
   const champName = await rli.question("Pick a champion (number or string) Master Yi default\n");
   const Champion: new() => Champion = champions[champName] || champions.find((champ) => new champ().name.toLowerCase() === champName.toLowerCase()) || MasterYi;
 
-  const countItemsStr = await rli.question("Count of items (1-6) 1 default\n");
-  let parsedCountItemsStr = Number(countItemsStr);
-  if (isNaN(parsedCountItemsStr)) parsedCountItemsStr = 1;
-  const countItems = Math.max(1, Math.min(6, Math.floor(parsedCountItemsStr)));
+  let itemsCount = parseInt(await rli.question("Count of items (1-6) 1 default\n"));
+  if (isNaN(itemsCount)) itemsCount = 1;
+  itemsCount = Math.max(1, Math.min(6, Math.floor(itemsCount)));
 
-  const levelStr = await rli.question("Select champion level (1-18) 9 default\n");
-  let parsedLevelStr = parseInt(levelStr);
-  if (isNaN(parsedLevelStr)) parsedLevelStr = 9;
-  const level = Math.max(1, Math.min(18, Math.floor(parsedLevelStr)));
+  let level = parseInt(await rli.question("Select champion level (1-18) 9 default\n"));
+  if (isNaN( level))  level = 9;
+  level = Math.max(1, Math.min(18, Math.floor(level)));
 
   const shouldRunAway = (await rli.question("Should dummy run away (y/n) n default\n"))[0] === "y" ? true : false;
 
-  //const withBoots = (await rli.question("Find best boots (y/n) n default\n"))[0] === "y" ? true : false;
+  rli.close();
+
+  return {
+    level,
+    shouldRunAway,
+    Champion,
+    itemsCount,
+    withBoots: false,
+  }
+}
+
+const getSetupBrowser = (): SimulateBestItemsSetup => {
+  let champNames = champions.reduce((prev, cur, i) => prev + `${i} ${new cur().name}\n`, "");
+  const champName = prompt(champNames + "Pick a champion (number or string) Master Yi default");
+  const Champion: new() => Champion = champions[champName] || champions.find((champ) => new champ().name.toLowerCase() === champName.toLowerCase()) || MasterYi;
+
+  let itemsCount = parseInt(prompt("Count of items (1-6) 1 default\n"));
+  if (isNaN(itemsCount)) itemsCount = 1;
+  itemsCount = Math.max(1, Math.min(6, Math.floor(itemsCount)));
+
+  let level = parseInt(prompt("Select champion level (1-18) 9 default\n"));
+  if (isNaN( level))  level = 9;
+  level = Math.max(1, Math.min(18, Math.floor(level)));
+
+  const shouldRunAway = (prompt("Should dummy run away (y/n) n default\n"))[0] === "y" ? true : false;
+
+  return {
+    level,
+    shouldRunAway,
+    Champion,
+    itemsCount,
+    withBoots: false,
+  }
+}
+
+const getSetup = async (): Promise<SimulateBestItemsSetup> => {
+  if (typeof window !== 'undefined') {
+    return getSetupBrowser();
+  } else {
+    return await getSetupNode();
+  }
+}
+
+const writeResult = (str: string) => {
+  if (typeof window !== 'undefined') {
+    document.body.innerText = str;
+  } else {
+    console.log(str);
+  }
+}
+
+(async () => {
+  const setup = await getSetup();
 
   const result = await simulateBestNextItems((sim) => {
-    const champ = new Champion();
-    champ.level = level;
+    const champ = new setup.Champion();
+    champ.level = setup.level;
     champ.init(sim);
     champ.levelUp();
     return champ;
-  }, countItems, shouldRunAway);
+  }, setup.itemsCount, setup.shouldRunAway);
 
+  let resultStr = "";
   if (result.length > 0) {
     for (const subresult of result) {
       for (const itemn in subresult.items) {
-        rli.write(`item${Number(itemn) + 1}: '${subresult.items[itemn].name}' `);
+        resultStr += `item${Number(itemn) + 1}: '${subresult.items[itemn].name}' `;;
       }
-      rli.write(`timetokill: ${(subresult.result.ttk / 1000).toFixed(2)} dps: ${subresult.result.dps1.toFixed(2)}\n`);
+      resultStr += `timetokill: ${(subresult.result.ttk / 1000).toFixed(2)} dps: ${subresult.result.dps1.toFixed(2)}\r\n`;
     }
   } else {
-    rli.write(`Simulation failed, no items simulated\n`);
+    resultStr += `Simulation failed, no items simulated\r\n`;
   }
 
-  rli.close();
+  writeResult(resultStr);
 })();
 
